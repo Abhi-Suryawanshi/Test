b9bb7be45570f42186b4dd2b6e0fa73f5a571190
Abhishek Suryawanshi <126605338+Abhi-Suryawanshi@users.noreply.github.com>
2025-02-22 21:57:16 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test {
     public static void main(String[] args) {
         System.out.print("Hello World");
-        System.out.print("Changed")
+        System.out.print("Changed");
             
     }
 }
